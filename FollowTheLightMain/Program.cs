9d2388bce1390e62730655bdf174132052398efb
Mustm4 <bastian.business@hotmail.com>
2024-02-05 11:50:46 +0100

    Continued with scenarios

--- FollowTheLightMain/Program.cs
+++ FollowTheLightMain/Program.cs
@@ -55,6 +55,15 @@ void Router(HttpListenerContext context)
                 case ("/game/player/1"):
                     GameGet(response);
                     break;
+                case ("/game/player/2"):
+                    GameTwoGet(response);
+                    break;
+                case ("/game/player/3"):
+                    GameThreeGet(response);
+                    break;
+                case ("/game/player/4"):
+                    GameFourGet(response);
+                    break;
                 default:
                     NotFound(response);
                     break; 
@@ -65,8 +74,13 @@ void Router(HttpListenerContext context)
             switch (request.Url?.AbsolutePath)
             {
                 case ("/game/player/1"):
-                    GamePost(request, response);
+                    GamePostOne(request, response);
                     break;
+                case ("/game/player/2"):
+                    GamePostTwo(request, response);
+                    break;
+
+                
             }
             break;
         
@@ -114,7 +128,7 @@ void GameGet(HttpListenerResponse response)
     response.OutputStream.Close();
 }
 
-void GamePost(HttpListenerRequest req, HttpListenerResponse res)
+void GamePostOne(HttpListenerRequest req, HttpListenerResponse res)
 {
     StreamReader reader = new(req.InputStream, req.ContentEncoding);
     string body = reader.ReadToEnd();
@@ -180,11 +194,57 @@ void GameTwoGet(HttpListenerResponse response)
     }
     response.OutputStream.Close();
 }
-// A. You almost chocked to death, stupid -1hp
-// B. Nothing happens, might regret that later
-// C. You burn yourself trying to light it up
-// D. There's writing on the paper in blood "DO NOT TRUST THE FROG"
 
+void GamePostTwo(HttpListenerRequest req, HttpListenerResponse res)
+{
+    StreamReader reader = new(req.InputStream, req.ContentEncoding);
+    string body = reader.ReadToEnd();
+    string answer = string.Empty;
+
+    switch (body)
+    {
+        case ("A"):
+        case ("a"):
+            answer +=
+                "You almost chocked to death, stupid. -1hp";
+            break;
+        case ("B"):
+        case ("b"):
+            answer +=
+                "Nothing happens, might regret that later.";
+            break;
+        case ("C"):
+        case ("c"):
+            answer +=
+                "You burn yourself trying to light it up.-1hp";
+            break;
+        case ("D"):
+        case ("d"):
+            answer +=
+                "There's writing on the paper in blood \"DO NOT TRUST THE FROG\"";
+            break;
+        default:
+            answer +=
+                "That option does not exist";
+            break;
+    }
+
+    byte[] buffer = Encoding.UTF8.GetBytes(answer);
+    res.ContentType = "text/plain";
+    res.StatusCode = (int)HttpStatusCode.OK;
+
+    foreach (byte b in buffer)
+    {
+        res.OutputStream.WriteByte(b);
+        Thread.Sleep(50);
+    }
+    res.OutputStream.Close();
+
+    Console.WriteLine($"Player answered the following to question one: {body}");
+
+    res.StatusCode = (int)HttpStatusCode.Created;
+    res.Close();
+}
 void GameThreeGet(HttpListenerResponse response)
 {
     string story = "You see a red frog sitting on a rock. It looks friendly even if there's bones around it..." +
@@ -213,6 +273,29 @@ void GameThreeGet(HttpListenerResponse response)
 
 // player two
 
+void GameFourGet(HttpListenerResponse response)
+{
+    string story = "You hear a sinister noise behind you..." +
+                     "Do you:\n\n" +
+                     "A. Make a shushing noise\n" +
+                     "B. Hide behind something\n" +
+                     "C. Look back and investiagte the noise\n" +
+                     "D. Scream and run";
+    byte[] buffer = Encoding.UTF8.GetBytes(story);
+    response.ContentType = "text/plain";
+    response.StatusCode = (int)HttpStatusCode.OK;
+
+    foreach (byte b in buffer)
+    {
+        response.OutputStream.WriteByte(b);
+        Thread.Sleep(50);
+    }
+    response.OutputStream.Close();
+}
+// A. You aggroad the monster and it attacks you. -1hp
+// B. You hide behind a rock, the monster passes you and walks away.
+// C. You bump into the monster and it scratches your face. -1hp
+// D. Your screaming exposes your position and it attacks you, you couldn't outrun the monster. -1hp
 void NotFound(HttpListenerResponse res)
 {
     res.StatusCode = (int)HttpStatusCode.NotFound;
