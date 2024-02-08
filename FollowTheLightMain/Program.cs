d9bb4ace17f1b0ec8ccc97a800b2f1c48daba031
matilda002 <matilda0817@hotmail.com>
2024-02-08 15:14:15 +0100

    deleted some gameGet and gamePost
    outdated, and not used in this state of the program

--- FollowTheLightMain/Program.cs
+++ FollowTheLightMain/Program.cs
@@ -66,19 +66,7 @@ async Task Router(HttpListenerContext context)
                     await GameOneGet(response);
                     break;
                 case ("/game/player/2"):
-                    GameTwoGet(response);
-                    break;
-                case ("/game/player/3"):
-                    GameThreeGet(response);
-                    break;
-                case ("/game/player/4"):
-                    GameFourGet(response);
-                    break;
-                case ("/game/player/5"):
-                    GameFiveGet(response);
-                    break;
-                case ("/game/player/10"):
-                    GameTenGet(response);
+                    await GameTwoGet(response);
                     break;
                 default:
                     await NotFound(response);
@@ -93,19 +81,10 @@ async Task Router(HttpListenerContext context)
                     await GameOnePost(request, response);
                     break;
                 case ("/game/player/2"):
-                    GameTwoPost(request, response);
+                    await GameTwoPost(request, response);
                     break;
-                case ("/game/player/3"):
-                    GameThreePost(request, response);
-                    break;
-                case ("/game/player/4"):
-                    GameFourPost(request, response);
-                    break;
-                case ("/game/player/5"):
-                    GameFivePost(request, response);
-                    break;
-                case ("/game/player/10"):
-                    GameTenPost(request, response);
+                default:
+                    await NotFound(response);
                     break;
             }
             break;
@@ -115,7 +94,6 @@ async Task Router(HttpListenerContext context)
             break;
     }
 }
-
 
 
 async Task IntroGet(HttpListenerResponse response)
@@ -165,8 +143,7 @@ async Task GameOneGet(HttpListenerResponse response)
     }
     response.OutputStream.Close();
 }
-
-void GameTwoGet(HttpListenerResponse response)
+async Task GameTwoGet(HttpListenerResponse response)
 {
     string story = "While you're walking through the tunnel you feel a piece paper under your feet, and pick it up." +
                      "Do you:\n\n" +
@@ -174,81 +151,6 @@ void GameTwoGet(HttpListenerResponse response)
                      "B. Throw it\n" +
                      "C. Burn it\n" +
                      "D. Read it";
-    byte[] buffer = Encoding.UTF8.GetBytes(story);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        response.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    response.OutputStream.Close();
-}
-void GameThreeGet(HttpListenerResponse response)
-{
-    string story = "You see a red frog sitting on a rock. It looks friendly even if there's bones around it..." +
-                     "Do you:\n\n" +
-                     "A. Burn it with the torch\n" +
-                     "B. Walk past the frog\n" +
-                     "C. Pick it up\n" +
-                     "D. Poke it with a bone from the ground";
-    byte[] buffer = Encoding.UTF8.GetBytes(story);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        response.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    response.OutputStream.Close();
-}
-void GameFourGet(HttpListenerResponse response)
-{
-    string story = "You hear a sinister noise behind you..." +
-                     "Do you:\n\n" +
-                     "A. Make a shushing noise\n" +
-                     "B. Hide behind something\n" +
-                     "C. Look back and investiagte the noise\n" +
-                     "D. Scream and run";
-    byte[] buffer = Encoding.UTF8.GetBytes(story);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        response.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    response.OutputStream.Close();
-}
-void GameFiveGet(HttpListenerResponse response)
-{
-    string story = "You find a wall, you need to climb over it...." +
-                     "Do you:\n\n" +
-                     "A. Climb the slippery wall\n" +
-                     "B. Look around the area\n" +
-                     "C. Climb the rope\n" +
-                     "D. Take the ladder with missing steps";
-    byte[] buffer = Encoding.UTF8.GetBytes(story);
-    response.ContentType = "text/plain";
-    response.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        response.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    response.OutputStream.Close();
-}
-void GameTenGet(HttpListenerResponse response)
-{
-    string story = "You found the exit.... but you hear someone screaming in the cave...." +
-                     "Do you:\n\n" +
-                     "A. Leave\n" +
-                     "B. Go back\n";
-
     byte[] buffer = Encoding.UTF8.GetBytes(story);
     response.ContentType = "text/plain";
     response.StatusCode = (int)HttpStatusCode.OK;
@@ -298,7 +200,7 @@ async Task GameOnePost(HttpListenerRequest req, HttpListenerResponse res)
     res.StatusCode = (int)HttpStatusCode.Created;
     res.Close();
 }
-void GameTwoPost(HttpListenerRequest req, HttpListenerResponse res)
+async Task GameTwoPost(HttpListenerRequest req, HttpListenerResponse res)
 {
     StreamReader reader = new(req.InputStream, req.ContentEncoding);
     string body = reader.ReadToEnd();
@@ -309,22 +211,12 @@ void GameTwoPost(HttpListenerRequest req, HttpListenerResponse res)
         case ("A"):
         case ("a"):
             answer +=
-                "You almost chocked to death, stupid. -1hp\n";
+                "You find teeth looks like it's human. You turn back around, take the left tunnel and find a torch.\n";
             break;
         case ("B"):
         case ("b"):
             answer +=
-                "Nothing happens, might regret that later.\n";
-            break;
-        case ("C"):
-        case ("c"):
-            answer +=
-                "You burn yourself trying to light it up.-1hp\n";
-            break;
-        case ("D"):
-        case ("d"):
-            answer +=
-                "There's writing on the paper in blood \"DO NOT TRUST THE FROG\"\n";
+                "You find a torch, might be useful later\n";
             break;
         default:
             answer +=
@@ -332,240 +224,16 @@ void GameTwoPost(HttpListenerRequest req, HttpListenerResponse res)
             break;
     }
 
+    //user choice
     byte[] buffer = Encoding.UTF8.GetBytes(answer);
     res.ContentType = "text/plain";
     res.StatusCode = (int)HttpStatusCode.OK;
-
     foreach (byte b in buffer)
     {
         res.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
+        Thread.Sleep(35);
     }
-
-    //jumpscare
-    string[] popup = File.ReadAllLines("popup-2.txt");
-    List<byte> byteList = new List<byte>();
-    foreach (string line in popup)
-    {
-        byte[] lineBytes = Encoding.UTF8.GetBytes(line + "\n");
-        byteList.AddRange(lineBytes);
-    }
-
-    byte[] test = byteList.ToArray();
-    foreach (byte x in test)
-    {
-        res.OutputStream.WriteByte(x);
-    }
-
-    res.OutputStream.Close();
-
-    Console.WriteLine($"Player answered the following to question one: {body}");
-
-    res.StatusCode = (int)HttpStatusCode.Created;
-    res.Close();
-}
-void GameThreePost(HttpListenerRequest req, HttpListenerResponse res)
-{
-    StreamReader reader = new(req.InputStream, req.ContentEncoding);
-    string body = reader.ReadToEnd();
-    string answer = string.Empty;
-
-    switch (body)
-    {
-        case ("A"):
-        case ("a"):
-            answer +=
-                "It screams in agony while it burns to death \n";
-            break;
-        case ("B"):
-        case ("b"):
-            answer +=
-                "You walk past the frog and continue on your path \n";
-            break;
-        case ("C"):
-        case ("c"):
-            answer +=
-                "The frog is poisonous, but it jumps out of your hand before damaging you critically. -1hp\n";
-            break;
-        case ("D"):
-        case ("d"):
-            answer +=
-                "You poke it with a stick, it makes a squeaking noise.\n";
-            break;
-        default:
-            answer +=
-                "That option does not exist\n";
-            break;
-    }
-
-    byte[] buffer = Encoding.UTF8.GetBytes(answer);
-    res.ContentType = "text/plain";
-    res.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        res.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-
-    //jumpscare
-    string[] popup = File.ReadAllLines("popup-3.txt");
-    List<byte> byteList = new List<byte>();
-    foreach (string line in popup)
-    {
-        byte[] lineBytes = Encoding.UTF8.GetBytes(line + "\n");
-        byteList.AddRange(lineBytes);
-    }
-
-    byte[] test = byteList.ToArray();
-    foreach (byte x in test)
-    {
-        res.OutputStream.WriteByte(x);
-    }
-
-    Console.WriteLine($"Player answered the following to question one: {body}");
-    res.OutputStream.Close();
-
-    res.StatusCode = (int)HttpStatusCode.Created;
-    res.Close();
-}
-void GameFourPost(HttpListenerRequest req, HttpListenerResponse res)
-{
-    StreamReader reader = new(req.InputStream, req.ContentEncoding);
-    string body = reader.ReadToEnd();
-    string answer = string.Empty;
-
-    switch (body)
-    {
-        case ("A"):
-        case ("a"):
-            answer +=
-                "You aggroad the monster and it attacks you. -1hp\n";
-            break;
-        case ("B"):
-        case ("b"):
-            answer +=
-                "You hide behind a rock, the monster passes you and walks away.\n";
-            break;
-        case ("C"):
-        case ("c"):
-            answer +=
-                "You bump into the monster and it scratches your face. -1hp\n";
-            break;
-        case ("D"):
-        case ("d"):
-            answer +=
-                "Your screaming exposes your position and it attacks you, you couldn't outrun the monster. -1hp\n";
-            break;
-        default:
-            answer +=
-                "That option does not exist\n";
-            break;
-    }
-
-    byte[] buffer = Encoding.UTF8.GetBytes(answer);
-    res.ContentType = "text/plain";
-    res.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        res.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    res.OutputStream.Close();
-
-    Console.WriteLine($"Player answered the following to question one: {body}");
-
-    res.StatusCode = (int)HttpStatusCode.Created;
-    res.Close();
-}
-void GameFivePost(HttpListenerRequest req, HttpListenerResponse res)
-{
-    StreamReader reader = new(req.InputStream, req.ContentEncoding);
-    string body = reader.ReadToEnd();
-    string answer = string.Empty;
-
-    switch (body)
-    {
-        case ("A"):
-        case ("a"):
-            answer +=
-                "You slipp and fall. -1hp\n";
-            break;
-        case ("B"):
-        case ("b"):
-            answer +=
-                "You find a secret passage, but you notice it was a dead end by walking into a wall. -1hp\n";
-            break;
-        case ("C"):
-        case ("c"):
-            answer +=
-                "The rope snaps in half and you fall. -1hp\n";
-            break;
-        case ("D"):
-        case ("d"):
-            answer +=
-                "You maanage to climb over the wall.\n";
-            break;
-        default:
-            answer +=
-                "That option does not exist\n";
-            break;
-    }
-
-    byte[] buffer = Encoding.UTF8.GetBytes(answer);
-    res.ContentType = "text/plain";
-    res.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        res.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    res.OutputStream.Close();
-
-    Console.WriteLine($"Player answered the following to question one: {body}");
-
-    res.StatusCode = (int)HttpStatusCode.Created;
-    res.Close();
-}
-void GameTenPost(HttpListenerRequest req, HttpListenerResponse res)
-{
-    StreamReader reader = new(req.InputStream, req.ContentEncoding);
-    string body = reader.ReadToEnd();
-    string answer = string.Empty;
-
-    switch (body)
-    {
-        case ("A"):
-        case ("a"):
-            answer +=
-                "You decided to leave - The End\n";
-            break;
-        case ("B"):
-        case ("b"):
-            answer +=
-                "You decided to go back deep into the dark cave and you notice that the screaming stopped... And turned into growling... - The End\n";
-            break;
-        default:
-            answer +=
-                "That option does not exist\n";
-            break;
-    }
-
-    byte[] buffer = Encoding.UTF8.GetBytes(answer);
-    res.ContentType = "text/plain";
-    res.StatusCode = (int)HttpStatusCode.OK;
-
-    foreach (byte b in buffer)
-    {
-        res.OutputStream.WriteByte(b);
-        Thread.Sleep(50);
-    }
-    res.OutputStream.Close();
-
-    Console.WriteLine($"Player answered the following to question one: {body}");
-
+    
     res.StatusCode = (int)HttpStatusCode.Created;
     res.Close();
 }
