6ebb1e5b49737362c4a2175ae03aab67831f8913
matilda002 <matilda0817@hotmail.com>
2024-02-14 10:32:49 +0100

    Rename files and added puzzles
    One naming standard for images and added two puzzles

--- FollowTheLightMain/DatabaseHelper.cs
+++ FollowTheLightMain/DatabaseHelper.cs
@@ -194,20 +194,23 @@ public class DatabaseHelper
     {
         Console.WriteLine("Populating the images table...");
 
-        string js1 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js-1.txt");
-        string js2 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js-2.txt");
-        string js3 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js-3.txt");
-        string js4 = File.ReadAllText($"FollowTheLightMain/images/Jumpscares/js-4.txt");
-        string js5 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js-5.txt");
-        string js6 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js-6.txt");
-        string imgFrog = File.ReadAllText($"FollowTheLightMain/images/frog.txt");
-        string imgStepStone = File.ReadAllText($"FollowTheLightMain/images/steppingstones.txt");
-        string imgStepStoneTable = File.ReadAllText($"FollowTheLightMain/images/steppingstonestable.txt");
-        string imgLock = File.ReadAllText($"FollowTheLightMain/images/lock.txt"); 
-        string imgGlowingWall = File.ReadAllText($"FollowTheLightMain/images/glowingwall.txt"); 
+        string js1 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js1.txt");
+        string js2 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js2.txt");
+        string js3 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js3.txt");
+        string js4 = File.ReadAllText($"FollowTheLightMain/images/Jumpscares/js4.txt");
+        string js5 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js5.txt");
+        string js6 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js6.txt");
+        string js7 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js7.txt");
+        string imgFrog = File.ReadAllText($"FollowTheLightMain/images/puzzles/frog.txt");
+        string imgStepStone = File.ReadAllText($"FollowTheLightMain/images/puzzles/stepstones.txt");
+        string imgStepStoneTable = File.ReadAllText($"FollowTheLightMain/images/puzzles/stepstonestable.txt");
+        string imgLock = File.ReadAllText($"FollowTheLightMain/images/puzzles/lock.txt"); 
+        string imgLockDoor = File.ReadAllText($"FollowTheLightMain/images/puzzles/lockdoor.txt"); 
+        string imgGlowingWall = File.ReadAllText($"FollowTheLightMain/images/puzzles/glowingwall.txt"); 
+        
 
         var cmd = _db.CreateCommand("insert into images(image)" +
-                                                "values ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11)");
+                                                "values ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12), ($13)");
         
         cmd.Parameters.AddWithValue($"{js1}");
         cmd.Parameters.AddWithValue($"{js2}");
@@ -215,10 +218,12 @@ public class DatabaseHelper
         cmd.Parameters.AddWithValue($"{js4}");
         cmd.Parameters.AddWithValue($"{js5}");
         cmd.Parameters.AddWithValue($"{js6}");
+        cmd.Parameters.AddWithValue($"{js7}");
         cmd.Parameters.AddWithValue($"{imgFrog}");
         cmd.Parameters.AddWithValue($"{imgStepStone}");
         cmd.Parameters.AddWithValue($"{imgStepStoneTable}");
         cmd.Parameters.AddWithValue($"{imgLock}");
+        cmd.Parameters.AddWithValue($"{imgLockDoor}");
         cmd.Parameters.AddWithValue($"{imgGlowingWall}");
         
         cmd.ExecuteNonQuery();
