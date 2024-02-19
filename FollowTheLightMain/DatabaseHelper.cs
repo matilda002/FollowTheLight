39b33487344a38eeb250a49a39365905f85ae302
lwcsharp <lwcsharp@gmail.com>
2024-02-19 13:16:39 +0100

    merge

--- FollowTheLightMain/DatabaseHelper.cs
+++ FollowTheLightMain/DatabaseHelper.cs
@@ -181,46 +181,6 @@ public class DatabaseHelper
         }
         return image;
     }
-    public void PopulateImagesTable()
-    {
-        Console.WriteLine("Populating the images table...\n\n");
-
-        string js1 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js1.txt");
-        string js2 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js2.txt");
-        string js3 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js3.txt");
-        string js4 = File.ReadAllText($"FollowTheLightMain/images/Jumpscares/js4.txt");
-        string js5 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js5.txt");
-        string js6 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js6.txt");
-        string js7 = File.ReadAllText($"FollowTheLightMain/images/jumpscares/js7.txt");
-        string imgFrog = File.ReadAllText($"FollowTheLightMain/images/frog.txt");
-        string imgStepStone = File.ReadAllText($"FollowTheLightMain/images/puzzles/stepstonesp2-stones.txt");
-        string imgStepStoneTable = File.ReadAllText($"FollowTheLightMain/images/puzzles/stepstonesp1-table.txt");
-        string imgLock = File.ReadAllText($"FollowTheLightMain/images/puzzles/lockp1-lock.txt");
-        string imgLockDoor = File.ReadAllText($"FollowTheLightMain/images/puzzles/lockp2-door.txt");
-        string imgGlowingWall = File.ReadAllText($"FollowTheLightMain/images/puzzles/wallp1-wall.txt");
-        string imgGlowingWallSign = File.ReadAllText($"FollowTheLightMain/images/puzzles/wallp2-sign.txt");
-
-
-        var cmd = _db.CreateCommand("insert into images(image)" +
-                                                "values ($1), ($2), ($3), ($4), ($5), ($6), ($7), ($8), ($9), ($10), ($11), ($12), ($13), ($14)");
-
-        cmd.Parameters.AddWithValue($"{js1}");
-        cmd.Parameters.AddWithValue($"{js2}");
-        cmd.Parameters.AddWithValue($"{js3}");
-        cmd.Parameters.AddWithValue($"{js4}");
-        cmd.Parameters.AddWithValue($"{js5}");
-        cmd.Parameters.AddWithValue($"{js6}");
-        cmd.Parameters.AddWithValue($"{js7}");
-        cmd.Parameters.AddWithValue($"{imgFrog}");
-        cmd.Parameters.AddWithValue($"{imgStepStone}");
-        cmd.Parameters.AddWithValue($"{imgStepStoneTable}");
-        cmd.Parameters.AddWithValue($"{imgLock}");
-        cmd.Parameters.AddWithValue($"{imgLockDoor}");
-        cmd.Parameters.AddWithValue($"{imgGlowingWall}");
-        cmd.Parameters.AddWithValue($"{imgGlowingWallSign}");
-
-        cmd.ExecuteNonQuery();
-    }
     
     public void PopulateTableStorypaths()
     {
